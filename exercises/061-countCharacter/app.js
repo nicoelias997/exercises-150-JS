a5f8345652cb6ddf0f80d8c21192466d038e897d
nicoelias997 <nicoelias997@gmail.com>
2022-07-19 21:29:37 +0200

    Ejercico 61, contador de letras

--- exercises/061-countCharacter/app.js
+++ exercises/061-countCharacter/app.js
@@ -1,3 +1,11 @@
 function countCharacter(str, char) {
-    // your code here
+    let contador = 0;
+    for(let i = 0; i < str.length; i++){
+        if(char == str[i]){
+            contador++  
+        }     
+    }
+  return contador;
 }
+var output = countCharacter('I am a hacker', 'a');
+console.log(output); // --> 3
