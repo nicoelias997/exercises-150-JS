5204bc6cb3946cedac46b0b8cebac5dea075c5d3
nicoelias997 <nicoelias997@gmail.com>
2022-07-19 21:32:09 +0200

    Ejercicio 62, dada una palabra, agregar cada letra a un array

--- exercises/062-getAllLetters/app.js
+++ exercises/062-getAllLetters/app.js
@@ -1,3 +1,9 @@
 function getAllLetters(str) {
-    // your code here
-}
+    let array = [];
+    for(let i = 0; i < str.length; i++){
+      array.push(str[i])
+    }
+      return array;
+  }
+  var output = getAllLetters('Radagast');
+  console.log(output); // --> ['R', 'a', 'd', 'a', 'g', 'a', 's', 't']
