0db00cf8b7d258498cc165a03c8cc5298f8948a7
nicoelias997 <nicoelias997@gmail.com>
2022-07-19 20:10:45 +0200

    Ejercicio 32, trabajamos con 2 objetos, el cual le asignamos a una propiedad del obj1, todo el obj2!

--- exercises/031-addFullNameProperty/app.js
+++ exercises/031-addFullNameProperty/app.js
@@ -1,3 +1,11 @@
+var person = {
+  firstName: 'Jade',
+  lastName: 'Smith'
+};
+
 function addFullNameProperty(obj) {
-    
+    obj.fullName = obj.firstName + " " + obj.lastName;
+  	return obj
 }
+addFullNameProperty(person);
+console.log(person.fullName);ss
--- exercises/032-addObjectProperty/app.js
+++ exercises/032-addObjectProperty/app.js
@@ -1,3 +1,14 @@
+var person1 = {
+  name: 'Joe Blow',
+  role: 'schlub'
+};
+var person2 = {
+  name: 'Mr. Burns',
+  role: 'supervisor'
+};
 function addObjectProperty(obj1, key, obj2) {
-  // tu codigo aqui
+    obj1[key] = obj2;
+    return obj1;
 }
+addObjectProperty(person1,"manager", person2);
+console.log(person1.manager)
