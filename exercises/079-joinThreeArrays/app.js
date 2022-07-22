b37c2113e34ffd9516d18d5526561fd0da690784
nicoelias997 <nicoelias997@gmail.com>
2022-07-22 19:10:00 +0200

    Resolvi el ejer 80, utilizo bucles anidados para poder llegar al array del objeto, y despues sumar los valores de su propiedad

--- exercises/079-joinThreeArrays/app.js
+++ exercises/079-joinThreeArrays/app.js
@@ -1,3 +1,8 @@
-function joinThreeArrays(arr1, arr2, arr3) {
-  // your code here
+const joinThreeArrays = (str1,str2,str3) => {
+  let strOne = str1.concat(str2);
+  let strFinal = strOne.concat(str3);
+  return strFinal;
 }
+
+var output = joinThreeArrays([1, 2], [3, 4], [5, 6]);
+console.log(output); // --> [1, 2, 3, 4, 5, 6]
--- exercises/080-getAverageOfElementsAtProperty/app.js
+++ exercises/080-getAverageOfElementsAtProperty/app.js
@@ -1,1 +1,18 @@
-// Write your function here
+var obj = {
+    key: [10, 10, 10]
+  };
+  
+  const getAverageOfElementsAtProperty = (obj,key) => {
+    let promedio = 0;
+      let suma = 0;
+    for(let key in obj){
+    for(let i = 0; i < obj[key].length; i++){
+      suma = suma + obj[key][i]
+      promedio = ( suma / obj[key].length)
+    }
+    }
+    return promedio;
+    
+  }
+  var output = getAverageOfElementsAtProperty(obj, 'key');
+  console.log(output); // --> 2
