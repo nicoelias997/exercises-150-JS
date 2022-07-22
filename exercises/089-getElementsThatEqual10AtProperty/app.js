a980ae43f8a3f80343e7b63b07e306e5c95e0501
nicoelias997 <nicoelias997@gmail.com>
2022-07-23 00:28:05 +0200

    Termine ejerc 89

--- exercises/089-getElementsThatEqual10AtProperty/app.js
+++ exercises/089-getElementsThatEqual10AtProperty/app.js
@@ -1,1 +1,23 @@
-// Write your function here
+var obj = {
+    key: [1000, 10, 50, 10, "", 13, 10]
+  };
+
+  const getElementsThatEqual10AtProperty = (obj,key) => {
+    let newObj = [];
+    for(let key in obj){
+      for(let i = 0; i < obj[key].length; i++){
+       if(obj[key][i] === 10){
+      	newObj[i] = obj[key][i];
+       } else{
+         newObj[i] = [];
+       }
+        
+       }
+      
+      }
+    return newObj
+    }
+    
+  
+  var output = getElementsThatEqual10AtProperty(obj, 'key');
+console.log(output); // --> [ [], 10, [], 10, [], [], 10]
