a640cf6ce200596b2406acae9bb22b69f1b5898a
nicoelias997 <nicoelias997@gmail.com>
2022-07-24 20:00:25 +0200

    Termine hasta el ejer 98, en este, hice otra correccion, en el cual debo llamar al obj[key], no a todo el objeto, deberia corregir esto tambien en ejercicios anteriores

--- exercises/097-keep/app.js
+++ exercises/097-keep/app.js
@@ -1,1 +1,11 @@
-// Write your function here
+const keep = (arr, element) => {
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i] !== element){
+        arr[i] = [];
+      }
+    }
+    return arr
+  }
+  
+  var output = keep([1, 2, 3, 2, 1], 2) 
+  console.log(output); //--> [ [], 2, [], 2, [] ]
--- exercises/098-getOddLengthWordsAtProperty/app.js
+++ exercises/098-getOddLengthWordsAtProperty/app.js
@@ -1,1 +1,22 @@
-// Write your function here
+var obj = {
+    key: ['It', 'has', 'some', 'words'],
+      paco: ['It', 'has', 'some', 'words']
+
+  };
+  const getOddLengthWordsAtProperty = (obj,key) => {
+    for(let propiedad in obj){
+      if(propiedad == key){
+        for(let i = 0; i < obj[key].length; i++){
+          if(obj[key][i].length % 2 !== 0){
+            obj[propiedad] = obj[key]
+          } else {
+            obj[key][i] = [];
+          }
+        }
+      }
+    }
+    return obj[key]
+  }
+  
+  var output = getOddLengthWordsAtProperty(obj, 'paco');
+  console.log(output); // --> ['has', 'words']
