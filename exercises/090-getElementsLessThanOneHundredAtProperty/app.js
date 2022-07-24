bf131f960ebf7b8e33d068238da53b70f33b683c
nicoelias997 <nicoelias997@gmail.com>
2022-07-24 18:43:02 +0200

    Termine ejercicio 90, igual al 89, solo que menor!

--- exercises/090-getElementsLessThanOneHundredAtProperty/app.js
+++ exercises/090-getElementsLessThanOneHundredAtProperty/app.js
@@ -1,1 +1,18 @@
-// Write your function here
+var obj = {
+    key: [1000, 20, 50, 500, 123, "asda",]
+  };
+  const getElementsLessThan100AtProperty = (obj,key) => {
+        let newObj = [];
+        for(let key in obj){
+        for(let i = 0; i < obj.key.length; i++){
+          if(obj.key[i] < 100){
+            newObj[i] = obj.key[i]
+          } else {
+            newObj[i] = [];
+          }
+        }
+      }
+        return newObj
+  }
+  var output = getElementsLessThan100AtProperty(obj, 'key');
+  console.log(output); // --> [ [], 20, 50, [], [], [] ]
