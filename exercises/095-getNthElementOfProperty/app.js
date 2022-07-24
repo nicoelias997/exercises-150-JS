ec6d1afd982b563c1ea91f992548edeed949a4d7
nicoelias997 <nicoelias997@gmail.com>
2022-07-24 19:41:13 +0200

    Termine ejercicio 96, y corregi lo que venia haciendo, debo recorrer el objeto, y debo poner una condicion de que !!SI propiedad == key, recorro key y desarrollo el problema, deberia corregir los ejercicios anteriores...

--- exercises/095-getNthElementOfProperty/app.js
+++ exercises/095-getNthElementOfProperty/app.js
@@ -1,1 +1,13 @@
-// Write your function here
+var obj = {
+    key: [1, 2, 6]
+  };
+  const getNthElementOfProperty = (obj,key,num) =>{
+    for(let key in obj){
+      for(let i = 0; i < obj[key].length; i++){
+        return obj[key][num]
+      }
+    }
+  }
+  
+  var output = getNthElementOfProperty(obj, 'key', 1);
+  console.log(output); // --> 2
--- exercises/096-getLastElementOfProperty/app.js
+++ exercises/096-getLastElementOfProperty/app.js
@@ -1,1 +1,15 @@
-// Write your function here
+var obj = {
+    paco:[21,132,31],
+    key: [1, 2, 5]
+  };
+  const getLastElementOfProperty = (obj,key) =>{
+    for(let propiedad in obj){
+      if(propiedad == key){
+        for(let i = 0; i < obj[key].length; i++){
+          return obj[key][obj[key].length -1]
+        }
+      }
+    }
+  }
+  var output = getLastElementOfProperty(obj, 'paco');
+  console.log(output); // --> 31
