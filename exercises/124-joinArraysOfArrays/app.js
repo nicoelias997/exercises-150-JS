b4ba13d43248fecc86be1de94e6f73fc4244714b
nicoelias997 <nicoelias997@gmail.com>
2022-07-25 22:46:16 +0200

    avance hasta el ej 125 sin problemas

--- exercises/124-joinArraysOfArrays/app.js
+++ exercises/124-joinArraysOfArrays/app.js
@@ -1,5 +1,10 @@
-function joinArrayOfArrays(arr) {
-  // your code here
+const joinArrayOfArrays = matriz => {
+  let array = [];
+  for(let i = 0; i < matriz.length; i++){
+    array = array.concat(matriz[i])
+  }
+  return array
 }
+
 var output = joinArrayOfArrays([[1, 4], [true, false], ['x', 'y']]);
 console.log(output); // --> [1, 4, true, false, 'x', 'y']
--- exercises/125-getProductOfAllElementsAtProperty/app.js
+++ exercises/125-getProductOfAllElementsAtProperty/app.js
@@ -1,9 +1,23 @@
 var obj = {
-  key: [1, 2, 3, 4]
+  key: [1,2,3,4]
 };
-
-function getProductOfAllElementsAtProperty(obj, key) {
-    // your code here
+const getProductOfAllElementsAtProperty = (obj,key) => {
+  let producto = 1;
+  for(let propiedad in obj){
+    if(propiedad == key){
+      if(obj[key].length == 0){
+        return 0
+      } else {
+      for(let i = 0; i < obj[key].length; i++){
+        producto = producto*obj[key][i]
+      	}
+      }
+    } else {
+      return 0
+    }
+  }
+  return producto
 }
+
 var output = getProductOfAllElementsAtProperty(obj, 'key');
 console.log(output); // --> 24
