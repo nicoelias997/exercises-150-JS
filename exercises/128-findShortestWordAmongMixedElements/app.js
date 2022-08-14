980e0692f63a2f798a3fa5a0acff3ff9969f4c56
nicoelias997 <nicoelias997@gmail.com>
2022-08-14 20:06:25 +0200

    Termine ejerc 127, retornar string mas corto dentro de un array

--- exercises/128-findShortestWordAmongMixedElements/app.js
+++ exercises/128-findShortestWordAmongMixedElements/app.js
@@ -1,5 +1,18 @@
-function findShortestWordAmongMixedElements(arr) {
-    // your code here
-}
-var output = findShortestWordAmongMixedElements([4, 'two', 2, 'three']);
-console.log(output); // --> 'two'
+const findShortestWordAmongMixedElements = (array) => {
+    let arrayStrings = [];
+    let shortWord = "";
+    for(let i = 0; i < array.length; i++){
+      if(typeof array[i] == "string"){
+        arrayStrings.push(array[i])
+      }
+    }
+    for(let i = 0; i < arrayStrings.length; i++){
+          shortWord = arrayStrings[0]
+      if(arrayStrings[i].length < arrayStrings[0].length){
+        shortWord = arrayStrings[i]
+      }
+    }  
+    return shortWord
+  }
+  var output = findShortestWordAmongMixedElements([4, 'two', 2, 'three',"abc"]);
+  console.log(output); // --> 'two'
