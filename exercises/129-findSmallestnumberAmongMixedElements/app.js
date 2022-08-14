ad275b36598e887c83073c2707747096b01955a2
nicoelias997 <nicoelias997@gmail.com>
2022-08-14 20:10:00 +0200

    Termine ej 129, retornar menor numero de un array que contiene varios tipos de datos

--- exercises/129-findSmallestnumberAmongMixedElements/app.js
+++ exercises/129-findSmallestnumberAmongMixedElements/app.js
@@ -1,6 +1,12 @@
 function findSmallestNumberAmongMixedElements(arr) {
-  // your code here
+  let numeros = [];
+  for(let i = 0; i < arr.length; i++){
+    if(!isNaN(arr[i])){
+      numeros.push(arr[i])
+    }
+  }
+return Math.min(...numeros)
 }
 
 var output = findSmallestNumberAmongMixedElements([4, 'lincoln', 9, 'octopus']);
-console.log(output); // --> 4
+console.log(output); // -->git 4
