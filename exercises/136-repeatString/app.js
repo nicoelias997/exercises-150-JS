daf928bc88947fff29aeb7c78939745ef94baef3
nicoelias997 <nicoelias997@gmail.com>
2022-08-14 20:43:11 +0200

    Termine ej 135, repetir una palabra dada, un numero de veces dado

--- exercises/136-repeatString/app.js
+++ exercises/136-repeatString/app.js
@@ -1,6 +1,8 @@
 function repeatString(string, num) {
-    // your code here
-}
-
-var output = repeatString('code', 3);
-console.log(output); // --> 'codecodecode'
+    if(typeof string == "string"){
+      return string.repeat(num)
+    }
+  }
+  
+  var output = repeatString('code', 3);
+  console.log(output); // --> 'codecodecode'
