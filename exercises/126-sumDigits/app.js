b72f9b2422f23e033bf388edbe88f34317f94f0e
nicoelias997 <nicoelias997@gmail.com>
2022-08-14 19:39:25 +0200

    Termine el ejercicio 126, mi duda era si ponia un numero -323-4 no me lo tomaba, pero no puede ser posible, por lo tanto esta bien la solucion

--- exercises/126-sumDigits/app.js
+++ exercises/126-sumDigits/app.js
@@ -1,5 +1,18 @@
 function sumDigits(num) {
-    // your code here
-}
-var output = sumDigits(316);
-console.log(output); // --> 4
+    let numero = num.toString().split("")
+    let sumaDigitos = 0;
+    for(let i = 0; i < numero.length; i++){
+      if(numero[i] == "-"){
+        i++
+      sumaDigitos -= parseInt(numero[i])
+        continue
+      }
+      sumaDigitos += parseInt(numero[i])
+    }
+    
+    return sumaDigitos
+  }
+  
+  sumDigits(-316); // -- 4
+  
+  sumDigits(1148);// --> 14
