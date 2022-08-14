e1334c199ae58eabe203e301d6fb63eba283c855
nicoelias997 <nicoelias997@gmail.com>
2022-08-14 20:17:56 +0200

    Termine ej 131, retornar mayor numero de un array que contiene varios tipos de datos

--- exercises/131-getLargestNumberAmongMixedElements/app.js
+++ exercises/131-getLargestNumberAmongMixedElements/app.js
@@ -1,6 +1,12 @@
 function getLargestNumberAmongMixedElements(arr) {
-    // your code here
-}
-
-var output = getLargestNumberAmongMixedElements([3, 'word', 5, 'up', 3, 1]);
-console.log(output); // --> 5
+    let numeros = [];
+    for(let i = 0;i < arr.length; i++){
+      if(!isNaN(arr[i])){
+        numeros.push(arr[i])
+      }
+    }
+    return Math.max(...numeros)
+  }
+  
+  var output = getLargestNumberAmongMixedElements([3, 'word', 5, 'up', 3, 1]);
+  console.log(output); // --> 5
