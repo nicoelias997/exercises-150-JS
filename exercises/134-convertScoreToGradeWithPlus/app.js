80c8b8d60fd36553d535e55a0df8cdb0a1b8be02
nicoelias997 <nicoelias997@gmail.com>
2022-08-14 20:34:47 +0200

    termine ej 134, retornar notas examenes con + o -, debia agregar mas condicionales dentro de los condicionales

--- exercises/134-convertScoreToGradeWithPlus/app.js
+++ exercises/134-convertScoreToGradeWithPlus/app.js
@@ -1,6 +1,54 @@
-function convertScoreToGradeWithPlusAndMinus(score) {
-    // your code here
+function convertScoreToGrade(score) {
+	let puntaje = "";
+  if(score < 0 || score > 100){
+    puntaje = "Invalid score"
+  }
+  if(score <= 100 && score >= 90){
+    puntaje = "A"
+    if(score < 93){
+      puntaje = "-A"
+    }
+    if(score > 98){
+      puntaje = "A+"
+    }
+  }
+  if(score < 90 && score >= 80){
+    puntaje = "B"
+     if(score < 83){
+      puntaje = "-B"
+    }
+    if(score > 88){
+      puntaje = "B+"
+    }
+  }
+  if(score < 80 && score >= 70){
+    puntaje = "C"
+     if(score < 73){
+      puntaje = "-C"
+    }
+    if(score > 78){
+      puntaje = "C+"
+    }
+  }
+  if(score < 70 && score >= 60){
+    puntaje = "D"
+     if(score < 63){
+      puntaje = "-D"
+    }
+    if(score > 68){
+      puntaje = "D+"
+    }
+  }
+  if(score > 0 && score < 60){
+    puntaje = "F"
+  }
+  
+  return puntaje
 }
 
-var output = convertScoreToGradeWithPlusAndMinus(91);
-console.log(output); // --> 'A-'
+convertScoreToGrade(99); //"A*"
+convertScoreToGrade(91); //"-A"
+convertScoreToGrade(70); //"-C"
+convertScoreToGrade(50); //"F"
+convertScoreToGrade(101); //"Invalid score"
+convertScoreToGrade(-2); //"Invalid score"
