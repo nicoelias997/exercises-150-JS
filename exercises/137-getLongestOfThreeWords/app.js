8c3fc58d183d43bdaab6c1f0cf4ef60382beb1ca
nicoelias997 <nicoelias997@gmail.com>
2022-08-14 20:57:08 +0200

    Termine ej 137, retornar la palabra mayor entre 3 palabras dadas

--- exercises/137-getLongestOfThreeWords/app.js
+++ exercises/137-getLongestOfThreeWords/app.js
@@ -1,6 +1,16 @@
 function getLongestOfThreeWords(word1, word2, word3) {
-    // your code here
-}
-
-var output = getLongestOfThreeWords('these', 'three', 'words');
-console.log(output); // --> 'these'
+    let mayorPalabra = "";
+      let palabras = [word1,  word2, word3]
+      for(let i = 0; i < palabras.length; i++){
+        mayorPalabra = palabras[0]
+        if(palabras[i].length > palabras[0].length){
+        mayorPalabra = palabras[i]  
+        }
+      }
+    return mayorPalabra
+  }
+  
+  var output = getLongestOfThreeWords('these', 'three', 'words');
+  console.log(output); // --> 'these'
+  
+  getLongestOfThreeWords("hola", "hola1", "hola12") // "hola12"
