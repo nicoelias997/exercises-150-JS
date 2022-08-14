2019383f4928e19ff7eedd6735904e89f3dbea3f
nicoelias997 <nicoelias997@gmail.com>
2022-08-14 20:29:43 +0200

    Termine ej 133,retornar notas de examenes, hice con condicionales

--- exercises/133-convertScoreToGrade/app.js
+++ exercises/133-convertScoreToGrade/app.js
@@ -1,6 +1,29 @@
 function convertScoreToGrade(score) {
-    // your code here
+	let puntaje = "";
+  if(score < 0 || score > 100){
+    puntaje = "Invalid score"
+  }
+  if(score <= 100 && score >= 90){
+    puntaje = "A"
+  }
+  if(score < 90 && score >= 80){
+    puntaje = "B"
+  }
+  if(score < 80 && score >= 70){
+    puntaje = "C"
+  }
+  if(score < 70 && score >= 60){
+    puntaje = "D"
+  }
+  if(score > 0 && score < 60){
+    puntaje = "F"
+  }
+  
+  return puntaje
 }
 
-var output = convertScoreToGrade(91);
-console.log(output); // --> 'A'
+convertScoreToGrade(91); //"A"
+convertScoreToGrade(70); //"C"
+convertScoreToGrade(50); //"F"
+convertScoreToGrade(101); //"Invalid score"
+convertScoreToGrade(-2); //"Invalid score"
