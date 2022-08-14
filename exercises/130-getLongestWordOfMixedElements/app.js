c337781e4a252714879cf94c886cf9bcb593502b
nicoelias997 <nicoelias997@gmail.com>
2022-08-14 20:15:53 +0200

    Termine ej 130, retornar palabra mas larga de un array que contiene varios tipos de datos

--- exercises/130-getLongestWordOfMixedElements/app.js
+++ exercises/130-getLongestWordOfMixedElements/app.js
@@ -1,6 +1,19 @@
 function getLongestWordOfMixedElements(arr) {
-    // your code here
-}
-
-var output = getLongestWordOfMixedElements([3, 'word', 5, 'up', 3, 1]);
-console.log(output); // --> 'word'
+    let arrString = [];
+    let longestWord = ""
+    for(let i = 0; i < arr.length; i++){
+      if(typeof arr[i] == "string"){
+            arrString.push(arr[i])
+      }
+    }
+    for(let i = 0; i < arrString.length; i++){
+      longestWord = arrString[0]
+      if(arrString[i].length > arrString[0].length){
+        longestWord = arrString[i]
+      }
+    }
+    return longestWord
+  }
+  
+  var output = getLongestWordOfMixedElements([3, 'word', 5, 'up', 3, 1, "aaa", "letras"]);
+  console.log(output); // --> 'letras'
