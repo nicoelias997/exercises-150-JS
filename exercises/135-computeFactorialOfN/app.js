055072d4207a389d64b5941145b6399d65bf963f
nicoelias997 <nicoelias997@gmail.com>
2022-08-14 20:40:25 +0200

    Termine ej 135, retornar factor de un nº entero, genero un num aleatorio entero, y lo multiplico por el num dado, que tambien lo hago entero

--- exercises/135-computeFactorialOfN/app.js
+++ exercises/135-computeFactorialOfN/app.js
@@ -1,7 +1,9 @@
 function computeFactorialOfN(n) {
-    // your code here
-}
-
-var output = computeFactorialOfN(3); console.log(output); // --> 6
-
-var output = computeFactorialOfN(4); console.log(output); // --> 24
+    let num = Math.round(n)
+    let numRandom = Math.floor((Math.random() * (10 - 1 + 1)) + 1);
+    return num*numRandom
+  }
+  
+  var output = computeFactorialOfN(3); console.log(output); // --> 6
+  
+  var output = computeFactorialOfN(4); console.log(output); // --> 24
